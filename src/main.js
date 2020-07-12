b280563172ffb4561849932e51b7273f9bc6e6d9
Vladimir-Vorobev <vovav2005@yandex.ru>
2020-07-12 21:18:25 +0300

    start

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+
+import MainPage from './components/MainPage.vue'
+
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: MainPage },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes, // сокращённая запись для `routes: routes`
+})
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
